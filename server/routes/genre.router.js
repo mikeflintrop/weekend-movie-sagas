63cd567cbd84b54ba17670d62105384cd1c1c300
Mike Flintrop <mike.flintrop@gmail.com>
2022-07-23 11:56:56 -0500

    added genre GET route

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -4,7 +4,15 @@ const pool = require('../modules/pool')
 
 router.get('/', (req, res) => {
   // Add query to get all genres
-  res.sendStatus(500)
+  console.log('in router.get');
+  const queryText = `SELECT * FROM "genres" ORDER BY "genres"."id";`;
+  pool.query(queryText)
+  .then(result => {
+    res.send(result.rows);
+  }).catch(error => {
+    console.log('error in router.get', error)
+    res.sendStatus(500);
+  })
 });
 
 module.exports = router;
